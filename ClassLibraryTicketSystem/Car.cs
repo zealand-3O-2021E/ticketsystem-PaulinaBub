93bdbdee7171a4ccce8e6e17a398e0b07196ea15
Paulina Bubnytė <bubneja@gmail.com>
2021-09-27 17:40:42 +0200

    First two classes

---
+++ ClassLibraryTicketSystem/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{
+    public class Car
+    {
+        public string Licenseplate;
+        public DateTime Date;
+
+        public double Price()
+        {
+            return 240;
+        }
+        public string VehicleType()
+        {
+            string type = "Car";
+            return type;
+        }
+    }
+}
---
+++ ClassLibraryTicketSystem/MC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{
+    public class MC
+    {
+        public string Licenseplate;
+        public DateTime Date;
+
+        public double Price()
+        {
+            return 125;
+        }
+        public string VehicleType()
+        {
+            string type = "MC";
+            return type;
+        }
+    }
+}
